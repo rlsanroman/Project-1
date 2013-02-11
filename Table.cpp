2a736dd03f6beda37f3472bf424c605a3f4a4743
Rosbel Sanroman <rlsanroman@tamu.edu>
2013-02-10 19:13:53 -0600

    Table Definitions
    Blank

--- Table.cpp
+++ Table.cpp
@@ -1,1 +1,51 @@
 #include "Table.h"
+
+Table::Table()
+{
+
+}
+Table::Table(vector<Attribute> a)
+{
+
+}
+
+void addColumn(Attribute att)
+{
+
+}
+
+void deleteColumn(string name)
+{
+
+}
+void insertRecord(Record r)
+{
+
+}
+
+
+
+void renameAttribute(string oldname, string newname)
+{
+	
+}
+void crossJoin(const Table& t1, const Table& t2)
+{
+
+}
+int sum(string name)
+{
+
+}
+unsigned int count(string name)
+{
+
+}
+int min (string name)
+{
+
+}
+int max (string name)
+{
+
+}
