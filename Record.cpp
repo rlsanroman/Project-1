0f698002f06cb37a6e105585a3ff39511b2c170d
Rosbel Sanroman <rlsanroman@tamu.edu>
2013-02-10 19:11:52 -0600

    Added Record Definitions

--- Record.cpp
+++ Record.cpp
@@ -1,1 +1,21 @@
 #include "Record.h"
+
+Record::Record()
+{
+	
+}
+
+Record::Record(vector<string> data)
+{
+
+}
+
+string Record::getTuple(int index)
+{
+
+}
+
+void Record::setTuple(int index, string newdata)
+{
+
+}
